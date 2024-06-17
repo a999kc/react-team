018c92532e259e8610c2df611f2a3f3fa6e2fb8d
Andrey Moskokov <asm2906@gmail.com>
2024-06-17 15:16:22 +0300

    done 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,3 @@
-// import React from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import RegisterForm from './components/RegisterForm';
@@ -41,7 +12,6 @@ const App: React.FC = () => {
         <Route path="/users" element={<UserList/>} />
         <Route path="/users/:userId" element={<UserSingle />} />
         <Route path="/login" element={<RegisterForm />} />
-        {/* Add more routes here for user list and user details */}
       </Routes>
     </Router>
   );
